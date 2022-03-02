c24cca586300b84b03cc0ce01176e8b03a89a079
Yi-Chi <88527554+YiChiChao@users.noreply.github.com>
2022-03-02 17:00:16 +0800

    Time: 124 ms (80.76%), Space: 20.4 MB (25.39%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.c
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.c
@@ -11,18 +11,12 @@ int* sortedSquares(int* nums, int numsSize, int* returnSize){
     while(left <= right){
         if(abs(nums[left]) > abs(nums[right])){
             ans[index--] = nums[left]*nums[left];
-            //printf("%d\n", nums[left]*nums[left]);
             ++left;
         }else {
             ans[index--] = nums[right]*nums[right];
-            //printf("%d\n", nums[right]*nums[right]);
-            //printf("v\n");
             --right;
-            //printf("v\n");
         }
     }
-    /*for(int i = 0; i < numsSize; ++i){
-        printf("%d ", ans[i]);
-    }printf("\n");*/
+    
     return ans;
 }
