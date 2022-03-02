542cd5642aa404ce10db7db22916ac86ea3cc8b3
Yi-Chi <88527554+YiChiChao@users.noreply.github.com>
2022-03-02 11:48:54 +0800

    Time: 7 ms (42.47%), Space: 6.1 MB (35.32%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.c
+++ 35-search-insert-position/35-search-insert-position.c
@@ -1,5 +1,7 @@
 int searchInsert(int* nums, int numsSize, int target){
     int left = 0, right = numsSize;
+    if(target > nums[right-1])return numsSize;
+    if(target < nums[0])return 0;
     while(left < right){
         int mid = (left+right)/2;
         if(nums[mid] > target){
