ff248299fcd871a635f9daa3e3174753ed802129
Yi-Chi <88527554+YiChiChao@users.noreply.github.com>
2022-03-13 22:39:18 +0800

    Time: 192 ms (30.20%), Space: 22.5 MB (18.63%) - LeetHub

--- 189-rotate-array/189-rotate-array.c
+++ 189-rotate-array/189-rotate-array.c
@@ -4,10 +4,8 @@ void rotate(int* nums, int numsSize, int k){
     int index = numsSize-k-1;
     for(int i = 0; i < numsSize; ++i){
         index = (index+1) % numsSize;
-        printf("%d \n", index);
-        printf("\n");
         ans[i] = nums[index];
-    }printf("\n");
+    }
     for(int i = 0; i < numsSize; ++i){
         nums[i] = ans[i];
     }
