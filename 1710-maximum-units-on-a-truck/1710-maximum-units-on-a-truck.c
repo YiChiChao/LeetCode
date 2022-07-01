e605fd10ef0e5c87c8ac864dff0754831150a956
Yi-Chi <88527554+YiChiChao@users.noreply.github.com>
2022-07-02 00:30:51 +0800

    Time: 32 ms (100.00%), Space: 7.6 MB (72.41%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.c
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.c
@@ -1,31 +1,23 @@
-int cmp (const void* a, const void* b){
-    int *A = *(int**)a;
-    int *B = *(int**)b;
-    return A[1] < B[1];
-}
 
 int maximumUnits(int** boxTypes, int boxTypesSize, int* boxTypesColSize, int truckSize){
-    qsort(boxTypes, boxTypesSize, sizeof(boxTypes[0]), cmp);
-    /*for(int i = 0; i < boxTypesSize; ++i){
-        printf("%d\n", boxTypes[i][1]);
-    }*/
-    *boxTypesColSize = 2;
+    int bucket[1001] = {0};
+    for(int i = 0; i < boxTypesSize; ++i){
+        bucket[boxTypes[i][1]] += boxTypes[i][0];
+    }
     int totalUnit = 0;
-    int numberOfBoxes, numberOfUnitsPerBox;
-    for(int i = 0; truckSize > 0 && i < boxTypesSize ; ++i){
-        numberOfBoxes = boxTypes[i][0];
-        numberOfUnitsPerBox = boxTypes[i][1];
-        if(truckSize >= numberOfBoxes){
-            totalUnit += numberOfBoxes * numberOfUnitsPerBox;
-            truckSize -= numberOfBoxes;
+    for(int i = 1000; i > 0 ; --i){
+        if(bucket[i] == 0)continue;
+        if(truckSize >= bucket[i]){
+            totalUnit += i * bucket[i];
+            truckSize -= bucket[i];
         }
         else{
-            totalUnit += truckSize * numberOfUnitsPerBox;
-            truckSize = 0;
+            totalUnit += truckSize * i;
+            return totalUnit;
         }
-        //printf("%d\n", totalUnit);
             
     }
     return totalUnit;
 }
+
     
