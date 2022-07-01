6768ba4080df99e4ff21b666f2b936cb16e1557a
Yi-Chi <88527554+YiChiChao@users.noreply.github.com>
2022-07-01 23:40:24 +0800

    Time: 57 ms (70.18%), Space: 17.7 MB (30.86%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(), boxTypes.end(), [](vector<int> a, vector<int> b){
-            return a[1] > b[1]; //decreasing
-        });
+        vector<int> bucket(1001, 0);
+        for(auto x: boxTypes){
+            bucket[x[1]] += x[0];
+        }
         int totalUnit = 0;
         int numberOfBoxes, numberOfUnitsPerBox;
-        for(int i = 0; truckSize > 0 && i < boxTypes.size() ; ++i){
-            numberOfBoxes = boxTypes[i][0];
-            numberOfUnitsPerBox = boxTypes[i][1];
-            if(truckSize >= numberOfBoxes){
-                totalUnit += numberOfBoxes * numberOfUnitsPerBox;
-                truckSize -= numberOfBoxes;
+        for(int i = 1000; i > 0 ; --i){
+            if(bucket[i] == 0)continue;
+            if(truckSize >= bucket[i]){
+                totalUnit += i * bucket[i];
+                truckSize -= bucket[i];
             }
             else{
-                totalUnit += truckSize * numberOfUnitsPerBox;
-                truckSize = 0;
+                totalUnit += truckSize * i;
+                return totalUnit;
             }
             
         }
