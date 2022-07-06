389c3779334b952b25ddc57b716128060ae9c393
Yi-Chi <88527554+YiChiChao@users.noreply.github.com>
2022-07-06 19:03:50 +0800

    Time: 9 ms (7.01%), Space: 7.6 MB (94.79%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -9,8 +9,9 @@ public:
         int Max = nums[0];
         int f1 = nums[1];
         int size = nums.size();
+        int temp;
         for(int i = 2; i < size; ++i){
-            int temp = f1;
+            temp = f1;
             f1 = max(Max + nums[i], f1);
             Max = max(Max, temp);
         }
