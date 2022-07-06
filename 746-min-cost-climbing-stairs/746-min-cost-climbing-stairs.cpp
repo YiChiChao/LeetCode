f4be5e0cb24d507df27efacad18c01718715f93d
Yi-Chi <88527554+YiChiChao@users.noreply.github.com>
2022-07-06 18:29:47 +0800

    Time: 7 ms (81.39%), Space: 13.6 MB (79.96%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,19 +4,18 @@ public:
         return (a < b) ? a:b;
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        int currentCost = 0;
-        int currentStep = 0;
-        vector<int> total(cost.size(), 0);
         //base case
-        total[0] = cost[0];
-        total[1] = cost[1];
+        int f0 = cost[0];
+        int f1 = cost[1];
         int stepLength = cost.size();
         
         //Recursive Form
         for(int i = 2 ; i < stepLength; ++i){
-            total[i] = cost[i] + min(total[i-1], total[i-2]);
+            int temp = f1;
+            f1 = cost[i] + min(f0, f1);
+            f0 = temp;
         }
         
-        return min(total[stepLength -1], total[stepLength -2]);
+        return min(f0, f1);
     }
 };
